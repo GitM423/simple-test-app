3b72ea5057bf5d6e7dab5213c7bc0ef4413eea99
GitM423 <matthias.p.hoeh@gmail.com>
2020-11-19 16:53:32 +0100

    v0.0.1

--- pages/user/index.js
+++ pages/user/index.js
@@ -1,18 +1,24 @@
 import { useContext } from "react";
 import UserContext from "../../components/Context/UserContext.component";
+import Layout from "../../components/Layout/Layout.component";
 // import UserContext from "../components/UserContext";
 
 const UserInfo = () => {
-  const { user } = useContext(UserContext);
+  const { user, login, logout } = useContext(UserContext);
 
   return (
-    <div className="user-info">
+    <Layout>
       <p>
         Hello, <strong>{user}</strong>
       </p>
       <p>Welcome to our app</p>
-      <button className="btn">Sign Out</button>
-    </div>
+      <button className="btn" onClick={login}>
+        Login
+      </button>
+      <button className="btn" onClick={logout}>
+        Logout
+      </button>
+    </Layout>
   );
 };
 
