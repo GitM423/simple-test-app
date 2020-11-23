e081102b7eb685d8d5f540d7bca2bd5246b36395
GitM423 <matthias.p.hoeh@gmail.com>
2020-11-23 12:19:28 +0100

    v0.0.3

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,27 +1,28 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 
 import { withRouter } from "next/router";
 
 import UserContext from "../../components/Context/UserContext.component";
 import Layout from "../../components/Layout/Layout.component";
 
-const Dashboard = () => {
-  const { loggedIn, user } = useContext(UserContext);
-
-  useEffect(() => {
-    if (!user) {
-      Router.push("/");
-    }
-  }, [user]);
-
+const Dashboard = (props) => {
   return (
     <Layout>
       <p>
-        Hello, <strong>{user?.user_metadata.full_name}</strong>
+        Hello, <strong>{props.user.user_metadata.full_name}</strong>
       </p>
       <p>Welcome to our app</p>
     </Layout>
   );
 };
+
+export async function getStaticProps() {
+  const { user } = useContext(UserContext);
+  if (!user) {
+    Router.push("/");
+  } else {
+    return { props: { user } };
+  }
+}
 
 export default withRouter(Dashboard);
